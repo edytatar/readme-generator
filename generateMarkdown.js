fc110ea2b520ad41704c08014b707a6cfbbf2bda
edytatar <edytatar@gmail.com>
2021-11-27 21:43:36 -0600

    Listed license, badges, and created README structure

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,115 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Creating an array of links to reference
+const arrLicenseLinks = [];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+        return `![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+    } else if (license === 'Apache 2.0') {
+        return `![Apache 2.0 License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+    } else if (license === 'BSD') {
+        return `![BSD License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
+    } else if (license === 'Unlicense') {
+        return `![Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`
+    } else {
+        return "";
+    }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+// Rreturns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+    if (license === "MIT") {
+        return `[MIT License](https://opensource.org/licenses/MIT)`;
+    } else if (license === "Apache 2.0") {
+        return `[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)`;
+    } else if (license === "BSD") {
+        return `[BSD (3-Clause) License](https://opensource.org/licenses/BSD-3-Clause)`;
+    } else if (license === "Unlicense") {
+        return `[Unlicense](http://unlicense.org/)`;
+    } else {
+        return "";
+    }
+}
 
-// TODO: Create a function to generate markdown for README
+// Adding License to Table of Contents if license is selected
+function licenseTableOfContents(license) {
+    if (license === "No License") {
+        return "";
+    } else {
+        return `* [License](#license)`;
+    }
+}
+
+// Creates license section title if a license was selected
+function renderLicenseSectionTitle(license) {
+    if (license === "No License") {
+        return "";
+    } else {
+        return `# License`; 
+    }
+
+}
+
+// Creates license section content if a license was selected
+function renderLicenseSectionContent(license) {
+    if (license === "No License") {
+        return "";
+    } else {
+        return `This project is licensed under the ${renderLicenseLink(license)}.`;
+    }
+
+}
+
+
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `# ${data.title} 
+
+  ${renderLicenseBadge(data.license)}
+  <br>
+
+  &nbsp;
+  ${data.description}
+
+  &nbsp;
+  # Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ${licenseTableOfContents(data.license)}
+  
+---
+  &nbsp;
+  ## Installation:
+  ${data.installation}
+
+  &nbsp;
+  ## Usage:
+  ${data.usage}
+
+  &nbsp;
+  ## Contributing:
+  ${data.contributing}
+
+  &nbsp;
+  ## Tests:
+  ${data.tests}
+
+  &nbsp;
+  ## Questions:
+  If you need any further assistance, my contact information is below.
+  * Github: [${data.username}](<https://github.com/${data.username}>)
+  
+  * Email: ${data.email}
+
+  &nbsp;
+${renderLicenseSectionTitle(data.license)}
+${renderLicenseSectionContent(data.license)}
 
 `;
 }
--- index.js
+++ index.js
@@ -6,137 +6,132 @@ const generateMarkdown = require('./generateMarkdown')
 
 // Array of questions for user input
 const questions = [
-    inquirer
-        .prompt([
-            {
-                // Project name
-                type: 'input',
-                message: 'What is the title of your project?',
-                name: 'title',
-                validate: function (title) {
-                    if (title) {
-                        return true;
-                    }
+    {
+        // Project name
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        validate: function (title) {
+            if (title) {
+                return true;
+            }
 
-                    return "A professional README needs a project title.";
-                },
-            },
-            {
-                // Project description
-                type: 'input',
-                message: 'Describe your application.',
-                name: 'description',
-                validate: function (description) {
-                    if (description) {
-                        return true;
-                    }
+            return "A professional README needs a project title.";
+        },
+    },
+    {
+        // Project description
+        type: 'input',
+        message: "Describe your application.",
+        name: 'description',
+        validate: function (description) {
+            if (description) {
+                return true;
+            }
 
-                    return "A professional README needs a description.";
-                },
-            },
-            {
-                // Installation instructions
-                type: 'input',
-                message: 'What are the installation instructions?',
-                name: 'installation',
-                validate: function (install) {
-                    if (install) {
-                        return true;
-                    }
+            return "A professional README needs a description.";
+        },
+    },
+    {
+        // Installation instructions
+        type: 'input',
+        message: "What are the installation instructions?",
+        name: 'installation',
+        validate: function (install) {
+            if (install) {
+                return true;
+            }
 
-                    return "A professional README needs to provide steps on how to install the project.";
-                },
-            },
-            {
-                // Application usage
-                type: 'input',
-                message: 'What is the usage of the application?',
-                name: 'usage',
-                validate: function (use) {
-                    if (use) {
-                        return true;
-                    }
+            return "A professional README needs to provide steps on how to install the project.";
+        },
+    },
+    {
+        // Application usage
+        type: 'input',
+        message: "What is the usage of the application?",
+        name: 'usage',
+        validate: function (use) {
+            if (use) {
+                return true;
+            }
 
-                    return "A professional README needs instructions on how to use the project.";
-                },
-            },
-            {
-                // Contribution directions
-                type: 'input',
-                message: 'How can someone contribute to this project?',
-                name: 'contribution',
-                validate: function (contribute) {
-                    if (contribute) {
-                        return true;
-                    }
+            return "A professional README needs instructions on how to use the project.";
+        },
+    },
+    {
+        // Contribution directions
+        type: 'input',
+        message: "How can someone contribute to this project?",
+        name: 'contribution',
+        validate: function (contribute) {
+            if (contribute) {
+                return true;
+            }
 
-                    return "A professional README needs contribution instructions.";
-                },
-            },
-            {
-                // Test instructions
-                type: 'input',
-                name: 'test',
-                message: 'Are there any test instructions you would like your contributors to follow?',
-                validate: function (testing) {
-                    if (testing) {
-                        return true;
-                    }
+            return "A professional README needs contribution instructions.";
+        },
+    },
+    {
+        // Test instructions
+        type: 'input',
+        name: 'test',
+        message: "Are there any test instructions you would like your contributors to follow?",
+        validate: function (testing) {
+            if (testing) {
+                return true;
+            }
 
-                    return "A professional README needs instructions on how to use the project.";
-                },
-            },
-            {
-                // License options
-                type: 'list',
-                name: 'license',
-                message: 'Pick a license:',
-                choices: ['MIT', 'Apache 2.0', 'BSD', 'No License'],
-            },
-            {
-                // Github username
-                type: 'input',
-                name: 'username',
-                message: 'What is your GitHub Username?',
-                validate: function (username) {
-                    if (username) {
-                        return true;
-                    }
+            return "A professional README needs instructions on how to use the project.";
+        },
+    },
+    {
+        // License options
+        type: 'list',
+        name: 'license',
+        message: "Pick a license:",
+        choices: ["MIT", "Apache 2.0", "BSD", "Unlicense", 'No License'],
+    },
+    {
+        // Github username
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub Username?",
+        validate: function (username) {
+            if (username) {
+                return true;
+            }
 
-                    return "Please enter your github username.";
-                },
-            },
-            {
-                // User's email
-                type: 'input',
-                name: 'email',
-                message: 'What is your email?',
-                validate: function (email) {
-                    let pass = email.match(/\S+@\S+\.\S+/g);
-                    if (pass) {
-                        return true;
-                    }
+            return "Please enter your github username.";
+        },
+    },
+    {
+        // User's email
+        type: 'input',
+        name: 'email',
+        message: "What is your email?",
+        validate: function (email) {
+            let pass = email.match(/\S+@\S+\.\S+/g);
+            if (pass) {
+                return true;
+            }
 
-                    return "Please enter a valid email.";
-                },
-            },
-        ])
+            return "Please enter a valid email.";
+        },
+    },
 ];
 
 // Function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
     const generateReadme = generateMarkdown(data);
     fs.writeFile("README.md", generateReadme, (err) => err ? console.log(err) : console.log("Your README.md has been generated."))
 };
 
 // Function to initialize app
 function init() {
-    function init() {
-        inquirer.prompt(questions)
-            .then((data) => {
-                writeToFile(data)
-            })
-    }
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile(data)
+        })
 }
 
 // Function call to initialize app
